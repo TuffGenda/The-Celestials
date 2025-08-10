699640b0656cecd9964698d0ec8d0cb28f0f2445
Elijah Martin <elijah.martin020201@gmail.com>
2025-08-09 21:26:35 -0400

    Initial Commit

---
+++ FPS Game/Assets/Misc_/CameraController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] int sens;
+    [SerializeField] int lockVertMin, lockVertMax;
+    [SerializeField] bool invertY;
+
+    InputAction lookAction;
+
+    float rotX;
+
+    private void Start()
+    {
+        lookAction = InputSystem.actions.FindAction("Player/Look");
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        Vector2 mouse = lookAction.ReadValue<Vector2>() * sens * Time.deltaTime;
+
+        if (invertY)
+        {
+
+            rotX += mouse.y;
+
+
+        }
+        else
+        {
+            rotX -= mouse.y;
+        }
+
+        rotX = Mathf.Clamp(rotX, lockVertMin, lockVertMax);
+
+        transform.localRotation = Quaternion.Euler(rotX, 0, 0);
+
+        transform.parent.Rotate(Vector3.up * mouse.x);
+    }
+}
